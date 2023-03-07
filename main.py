f5fe06d43e9c79aee1ba4974581095ab57b5b07b
Maksim59 <48496907+Maksim59@users.noreply.github.com>
2023-03-06 22:35:47 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,222 @@
+# import required modules
+#YOU NEED TO DOWNLOAD PLAYSOUND ON PIP OR ON YOUR IDE
+import turtle
+import time
+import random
+from playsound import playsound
+from tkinter import *
+root = Tk()
+root.geometry('900x700')
+root.configure(bg='light blue')
+
+
+
+colours = []
+def green_click():
+    global colours
+    colours.append('green')
+    root.quit()
+def orange_click():
+    colours.append('orange')
+    root.quit()
+def purple_click():
+    colours.append('purple')
+    root.quit()
+
+x,y = 50,500
+
+label = Label(root, text="Choose which colour you want your snake to be", bg='light blue', fg='Black', font = ('Calibri', 30, 'normal'))
+label.pack()
+label.place(x=x,y=y)
+Green = Button(root, text="Green Snake", padx=50, pady=50, command=green_click,bg='green',fg='white')
+Green.pack()
+Orange = Button(root, text="Orange Snake", padx=50, pady=50, command=orange_click,bg='orange',fg='white')
+Orange.pack()
+Purple = Button(root, text="Purple Snake", padx=50, pady=50, command=purple_click,bg='purple', fg='white')
+Purple.pack()
+
+
+
+root.mainloop()
+
+
+MAINCOLOUR = colours[0]
+
+
+
+delay = 0.1
+score = 0
+high_score = 0
+
+
+
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+wn = turtle.Screen()
+
+wn.title("Snake Game")
+wn.bgcolor("black")
+
+# the width and height can be put as user's choice
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+# head of the snake
+head = turtle.Turtle()
+head.shape('square')
+head.color(MAINCOLOUR)
+head.penup()
+head.goto(0, 0)
+head.direction = "Stop"
+
+
+# food in the game
+food = turtle.Turtle()
+
+food.speed(0)
+food.shape("square")
+food.color("red")
+food.penup()
+food.goto(0, 100)
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 250)
+pen.write("Score : 0  High Score : 0", align="center",
+          font=("candara", 24, "bold"))
+
+
+
+
+# assigning key directions
+def goup():
+    if head.direction != "down":
+        head.direction = "up"
+
+
+def godown():
+    if head.direction != "up":
+        head.direction = "down"
+
+
+def goleft():
+    if head.direction != "right":
+        head.direction = "left"
+
+
+def goright():
+    if head.direction != "left":
+        head.direction = "right"
+
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+
+wn.listen()
+wn.onkeypress(goup, "w")
+wn.onkeypress(godown, "s")
+wn.onkeypress(goleft, "a")
+wn.onkeypress(goright, "d")
+
+segments = []
+
+
+# Main Gameplay
+while True:
+
+    wn.update()
+    if score == 0:
+        head.color(MAINCOLOUR)
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "Stop"
+        colors = MAINCOLOUR
+        shapes = 'square'
+        for segment in segments:
+            segment.goto(1000, 1000)
+        segments.clear()
+        score = 0
+        delay = 0.1
+        pen.clear()
+        pen.write("Score : {} High Score : {} ".format(
+            score, high_score), align="center", font=("candara", 24, "bold"))
+
+    if head.distance(food) < 20:
+        playsound(r"snakepickup1.mp3")
+        x = random.randint(-270, 270)
+        y = random.randint(-270, 270)
+        food.goto(x, y)
+
+
+        # Adding segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color(MAINCOLOUR)  # tail colour
+        new_segment.penup()
+        segments.append(new_segment)
+        delay -= 0.001
+        score += 1
+        if score > high_score:
+            high_score = score
+        pen.clear()
+        pen.write("Score : {} High Score : {} ".format(
+            score, high_score), align="center", font=("candara", 24, "bold"))
+
+    # Checking for head collisions with body segments
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+    move()
+    for segment in segments:
+        if segment.distance(head) < 15:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+            colors = MAINCOLOUR
+            shapes = random.choice(['square', 'circle'])
+            for segment in segments:
+                segment.goto(1000, 1000)
+            segment.clear()
+
+            score = 0
+            delay = 0.1
+            pen.clear()
+            pen.write("Score : {} High Score : {} ".format(
+                score, high_score), align="center", font=("candara", 24, "bold"))
+    time.sleep(delay)
+
+    if score > 30:
+        head.color('white')
+
+    if score > 50:
+        head.color('light blue')
+    if score > 100:
+        head.color('red')
+
+
+wn.mainloop()
